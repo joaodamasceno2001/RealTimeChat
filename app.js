143571db5df64e3e6794223c26a107608d003bd6
João Victor Damasceno <joao_damasceno2001@hotmail.com>
2021-03-16 15:17:39 -0300

    Metade projeto finalizado

---
+++ app.js
@@ -0,0 +1,7 @@
+/* importar as cofigurações do servidor */
+var app = require('./config/server');
+
+/* parametrizar a porta de escuta */
+app.listen(80, function(){
+    console.log('Servidor Online')
+})
